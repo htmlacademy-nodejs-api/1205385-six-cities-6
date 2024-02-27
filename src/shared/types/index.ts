1c466862cec5a23c7ba44ab17c7d90c9ff406531
@danilko.a.g <danilko.a.g@gmail.com>
2024-02-27 20:51:36 +0100

    add new methods for offer service

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -7,4 +7,4 @@ export * from './location.type.js';
 export * from './offer-type.type.js';
 export * from './mock-server-data.type.js';
 export * from './component.enum.js';
-
+export * from './sort-type.enum.js';
